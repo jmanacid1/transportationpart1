c588c61356ae476554ba6d50468468e38d3c8414
jmanacid1 <jacksonwlipfert@gmail.com>
2015-10-18 14:04:51 -0400

    Transportation part 1

---
+++ Assets/CubeBehavior.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeBehavior : MonoBehaviour {
+	
+
+
+	void OnMouseDown () {
+		foreach (GameObject position in gameController.allCubes) {
+			position.GetComponent<Renderer> ().material.color = Color.white;
+		}
+		GetComponent<Renderer> ().material.color = Color.red;
+	}
+
+}
---
+++ Assets/gameController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class gameController : MonoBehaviour {
+	int numCubes = 16;
+	public GameObject cubePrefab;
+	public static GameObject [] allCubes;
+		
+	// Use this for initialization
+	void Start () {
+		allCubes = new GameObject[numCubes];
+		for (int i = 0; i < numCubes; i++) {
+			allCubes [i] = (GameObject)Instantiate(cubePrefab, new Vector3(i*2, 0, 0), Quaternion.identity);
+		}
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
